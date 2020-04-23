cb4226d0f4caca8e1571412919cfdc901458469f
Student_M7 <Student_M7@PD-DAL-M7.local>
2020-04-23 03:50:49 -0400

    #11

--- fibn/src/components/layout/SearchBox.js
+++ fibn/src/components/layout/SearchBox.js
@@ -1,9 +1,31 @@
 import React from 'react'
 
-export default function SearchBox() {
-    return (
-        <div>
-            
-        </div>
-    )
+class SearchBox extends Component {
+    state = {
+        search:""
+      };
+
+    onchange = e => {
+        this.setState({search: e.target.value});
+    };
+
+    render() { 
+        const {search} = this.state;
+        return (
+            <center>
+                <div className ="SearchR">
+                    <Input 
+                        label=" Search Critaria Goes Here"
+                        icon=" search"
+                        onChange={this.onchange}
+                    />
+                </div>
+            </center>
+          );
+    }
 }
+
+
+
+ 
+export default SearchBox;
