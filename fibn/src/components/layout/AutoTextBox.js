bcf8432eac8671e6ff9a41655c00d234006c8bf0
Student_M7 <Student_M7@PD-DAL-M7.local>
2020-04-24 19:19:18 -0400

    21

--- fibn/src/components/layout/AutoTextBox.js
+++ fibn/src/components/layout/AutoTextBox.js
@@ -4,17 +4,48 @@ class AutoTextBox extends React.Component{
     constructor (props){
         super(props);
         this.items=[
-            // 'get.from database',
+            'kyulk snyt',
+            'SFtnyt',
+            'wagrnyt',
+            'y8ott',
+            'BXWFf 6 snyt',
+            'sdgdsnyt'
         ];
+      
+        this.state={
+            suggestions:[],
+        };
     }
+
+    onTextChanged = (e) => {
+        const value = e.target.value;
+        let suggestions = [];
+        if (value.length > 0){
+            const regex = new RegExp(`^${value}`,'i');
+            suggestions = this.items.sort().filter(v=>regex.test(v));
+        }
+        this.setState(()=> ({suggestions}));
+    }
+
+    renderSugeestions(){
+        const{suggestions}=this.state;
+        if (suggestions.length === 0){
+            return null;
+        }
+        return(
+            <ul>
+                {suggestions.map((item) => <li>{item}</li>)}
+            </ul>
+        )
+
+    }
+
 
     render(){
         return(
             <div className ="AutoTextBox">
-                <input type="text" placeholder="be inspired: search here   " name="uName" required/>
-                <ul>
-                    {this.items.map((item) => <li>{item}</li>)}
-                </ul>
+                <input onChange={this.OnTextChanged} type="text" placeholder="be inspired: search here   " name="uName" required/>
+                {this.renderSuggestions()}
             </div>
         );
     };
